3b4c19e5bab1b38b9aebc1076f931a1fb08172a1
Rumit m Gevariya <77478366+RumitGevariya@users.noreply.github.com>
2021-01-15 17:19:26 +0530

    Create os and system .py

---
+++ os and system .py
@@ -0,0 +1,14 @@
+#for example
+import os
+# print(dir(os))
+import math
+#print(dir(math))
+# print(os.getcwd())
+f =open("filsse.txt",'r')
+#os.chdir("C:\\Users\DellX")
+#print(os.getcwd())
+#print(os.listdir("C://"))
+# os.mkdir("rumit")
+#os.makedirs("this/that")
+os.rename("filsse.txt","rumitspatel.txt")
+#os.path.join("C://","remit.txt")
